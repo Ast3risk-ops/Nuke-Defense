9057400a031c9ac476f137f6db5a1ff0c3aef4d4
spacedude609 <jwmidds@outlook.com>
2021-08-07 09:56:03 -0400

    Create nuke_defense.py

---
+++ nuke_defense.py
@@ -0,0 +1,183 @@
+from time import sleep
+from os import system
+# Look at Python project.txt
+system('color 0A')
+print("The year is 1985")
+sleep(1)
+print("Ro(n)(a)ld Regan i(s) president of the Unied States Of (a)merica")
+sleep(2)
+print("You are the captain of a nuclear submarine carrying nukes ((d)uh)")
+sleep(2)
+print("You use the co(m)puter")
+sleep(1)
+print("The Nuke PC (i)s Loadi(n)g...")
+sleep(4)
+print()
+sleep(0.5)
+print("---------------------------------")
+sleep(0.1)
+print("---------------------------------")
+sleep(0.1)
+print("WELCOME TO THE NUKE PC")
+sleep(0.1)
+print("---------------------------------")
+sleep(0.1)
+print("From here you can launch nukes")
+sleep(0.1)
+print("---------------------------------")
+print()
+sleep(1)
+print()
+# Login script
+while True:
+    print("Login please.")
+    username = input("username> ")
+    if username == "nasadmin":
+        password = input("password> ")
+        if password == "password":
+            print("Login Success!")
+            break
+        else:
+            print("Try again")
+            print("Hint: What is one of the most common passwords?")
+            sleep(2)
+    else:
+        print("Try again")
+        print("Hmm... Strange... I can't find the username anywhere...")
+        sleep(1)
+        print("Is it up there?")
+        sleep(2)
+print("You have succesfully logged in!")
+sleep(1)
+print()
+# ASCII art
+print("/$$      /$$ /$$$$$$$$ /$$        /$$$$$$   /$$$$$$  /$$      /$$ /$$$$$$$$")
+sleep(0.1)
+print(" $$  /$ | $$| $$_____/| $$       /$$__  $$ /$$__  $$| $$$    /$$$| $$_____/")
+sleep(0.1)
+print("| $$ /$$$| $$| $$      | $$      | $$  \__/| $$  \ $$| $$$$  /$$$$| $$      ")
+sleep(0.1)
+print("| $$/$$ $$ $$| $$$$$   | $$      | $$      | $$  | $$| $$ $$/$$ $$| $$$$$   ")
+sleep(0.1)
+print("| $$$$_  $$$$| $$__/   | $$      | $$      | $$  | $$| $$  $$$| $$| $$__/   ")
+sleep(0.1)
+print("| $$$/ \  $$$| $$      | $$      | $$    $$| $$  | $$| $$\  $ | $$| $$      ")
+sleep(0.1)
+print("| $$/   \  $$| $$$$$$$$| $$$$$$$$|  $$$$$$/|  $$$$$$/| $$ \/  | $$| $$$$$$$$")
+sleep(0.1)
+print("|__/     \__/|________/|________/ \______/  \______/ |__/     |__/|________/")
+print()
+sleep(0.1)
+print("nasadmin")
+sleep(2)
+print("Loading info... ")
+sleep(4)
+print()
+print()
+print("------")
+sleep(0.1)
+print("------")
+sleep(0.1)
+print("ALERT!")
+sleep(0.1)
+print("------")
+sleep(0.1)
+print("------")
+sleep(0.1)
+print()
+sleep(3)
+# Based off of what I know about Regan
+print("A huge asteroid is coming to hit earth")
+sleep(2)
+print("And communist aliens are coming off of it")
+sleep(2)
+print("Psst! In case you don't know Regan calls communism an evil empire")
+sleep(2)
+print("The Nuclear Football has been opened")
+sleep(2)
+print("You must launch the nuke!")
+sleep(2)
+print()
+print("------------")
+sleep(0.1)
+print("------------")
+sleep(0.1)
+print("INSTRUCTIONS")
+sleep(0.1)
+print("------------")
+sleep(0.1)
+print("------------")
+sleep(0.1)
+print()
+print("To launch the nuke, you must turn a key in a lock simultaniously with one or more of your crewmates.")
+sleep(6)
+print("-END INSTRUCTIONS-")
+sleep(2)
+print("There are 6 people on board including the captain")
+sleep(2)
+print("However there has been a mutiny on board.")
+sleep(2)
+print("4 of your crew have sided with the aliens.")
+sleep(2)
+print("The other crew member has been taken hostage")
+sleep(2)
+print("You are holed up in the pilot area which has the mechanism to launch the nuke.")
+sleep(4)
+print("The barricade won't hold up for long")
+sleep(2)
+print("They are coming")
+sleep(2)
+print("You have your key on you now")
+sleep(2)
+print("You have 2 choices.")
+sleep(1)
+print("You may:")
+sleep(1)
+print("1) Try and turn the key to launch the nuke")
+sleep(1)
+print("2) Attempt to rescue the hostage")
+sleep(1)
+print("To choose type 1 or 2")
+choose1 = input("What shall you do? ")
+if choose1 == "1":
+    print("You take your key and turn it in the lock")
+    sleep(2)
+    print("Nothing happens")
+    sleep(2)
+    print("The aliens break open the barricade")
+    system('color 04')
+    sleep(2)
+    print("They kill you and the other crew memeber")
+    sleep(2)
+    print("""
+
+
+
+      _____                   _______                   _____                            _____                    _____                    _____                    _____                            _____                    _____                    _____                    _____
+     |\    \                 /::\    \                 /\    \                          /\    \                  /\    \                  /\    \                  /\    \                          /\    \                  /\    \                  /\    \                  /\    \
+     |:\____\               /::::\    \               /::\____\                        /::\____\                /::\    \                /::\____\                /::\    \                        /::\    \                /::\    \                /::\    \                /::\    \
+     |::|   |              /::::::\    \             /:::/    /                       /:::/    /               /::::\    \              /:::/    /               /::::\    \                      /::::\    \               \:::\    \              /::::\    \              /::::\    \
+     |::|   |             /::::::::\    \           /:::/    /                       /:::/    /               /::::::\    \            /:::/    /               /::::::\    \                    /::::::\    \               \:::\    \            /::::::\    \            /::::::\    \
+     |::|   |            /:::/~~\:::\    \         /:::/    /                       /:::/    /               /:::/\:::\    \          /:::/    /               /:::/\:::\    \                  /:::/\:::\    \               \:::\    \          /:::/\:::\    \          /:::/\:::\    \
+     |::|   |           /:::/    \:::\    \       /:::/    /                       /:::/____/               /:::/__\:::\    \        /:::/____/               /:::/__\:::\    \                /:::/  \:::\    \               \:::\    \        /:::/__\:::\    \        /:::/  \:::\    \
+     |::|   |          /:::/    / \:::\    \     /:::/    /                       /::::\    \              /::::\   \:::\    \       |::|    |               /::::\   \:::\    \              /:::/    \:::\    \              /::::\    \      /::::\   \:::\    \      /:::/    \:::\    \
+     |::|___|______   /:::/____/   \:::\____\   /:::/    /      _____            /::::::\    \   _____    /::::::\   \:::\    \      |::|    |     _____    /::::::\   \:::\    \            /:::/    / \:::\    \    ____    /::::::\    \    /::::::\   \:::\    \    /:::/    / \:::\    \
+     /::::::::\    \ |:::|    |     |:::|    | /:::/____/      /\    \          /:::/\:::\    \ /\    \  /:::/\:::\   \:::\    \     |::|    |    /\    \  /:::/\:::\   \:::\    \          /:::/    /   \:::\ ___\  /\   \  /:::/\:::\    \  /:::/\:::\   \:::\    \  /:::/    /   \:::\ ___\
+    /::::::::::\____\|:::|____|     |:::|    ||:::|    /      /::\____\        /:::/  \:::\    /::\____\/:::/  \:::\   \:::\____\    |::|    |   /::\____\/:::/__\:::\   \:::\____\        /:::/____/     \:::|    |/::\   \/:::/  \:::\____\/:::/__\:::\   \:::\____\/:::/____/     \:::|    |
+   /:::/~~~~/~~       \:::\    \   /:::/    / |:::|____\     /:::/    /        \::/    \:::\  /:::/    /\::/    \:::\  /:::/    /    |::|    |  /:::/    /\:::\   \:::\   \::/    /        \:::\    \     /:::|____|\:::\  /:::/    \::/    /\:::\   \:::\   \::/    /\:::\    \     /:::|____|
+  /:::/    /           \:::\    \ /:::/    /   \:::\    \   /:::/    /          \/____/ \:::\/:::/    /  \/____/ \:::\/:::/    /     |::|    | /:::/    /  \:::\   \:::\   \/____/          \:::\    \   /:::/    /  \:::\/:::/    / \/____/  \:::\   \:::\   \/____/  \:::\    \   /:::/    /
+ /:::/    /             \:::\    /:::/    /     \:::\    \ /:::/    /                    \::::::/    /            \::::::/    /      |::|____|/:::/    /    \:::\   \:::\    \               \:::\    \ /:::/    /    \::::::/    /            \:::\   \:::\    \       \:::\    \ /:::/    /
+/:::/    /               \:::\__/:::/    /       \:::\    /:::/    /                      \::::/    /              \::::/    /       |:::::::::::/    /      \:::\   \:::\____\               \:::\    /:::/    /      \::::/____/              \:::\   \:::\____\       \:::\    /:::/    /
+\::/    /                 \::::::::/    /         \:::\__/:::/    /                       /:::/    /               /:::/    /        \::::::::::/____/        \:::\   \::/    /                \:::\  /:::/    /        \:::\    \               \:::\   \::/    /        \:::\  /:::/    /
+ \/____/                   \::::::/    /           \::::::::/    /                       /:::/    /               /:::/    /          ~~~~~~~~~~               \:::\   \/____/                  \:::\/:::/    /          \:::\    \               \:::\   \/____/          \:::\/:::/    /
+                            \::::/    /             \::::::/    /                       /:::/    /               /:::/    /                                     \:::\    \                       \::::::/    /            \:::\    \               \:::\    \               \::::::/    /
+                             \::/____/               \::::/    /                       /:::/    /               /:::/    /                                       \:::\____\                       \::::/    /              \:::\____\               \:::\____\               \::::/    /
+                              ~~                      \::/____/                        \::/    /                \::/    /                                         \::/    /                        \::/____/                \::/    /                \::/    /                \::/____/
+                                                       ~~                               \/____/                  \/____/                                           \/____/                          ~~                       \/____/                  \/____/                  ~~
+
+""")
+    sleep(2)
+    print("To be annoying there is no restart button")
+    sleep(2)
+    print("Restart the script to restart")
+    sleep(4)
